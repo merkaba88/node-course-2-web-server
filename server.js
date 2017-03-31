f80046d562923dda947341164a539a0f99df31e6
Daniel Fegan <daniel.fegan1988@gmail.com>
2017-03-31 11:35:28 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+// Set up http route handlers
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello express</h1>');
+  res.render('home', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Sup'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fulfill this request'
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+});
